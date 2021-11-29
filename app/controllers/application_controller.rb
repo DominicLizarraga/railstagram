dd0dbc4961f01c3f6916c8cb360146bb41b811b8
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-11-28 21:59:16 -0800

    added active storage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :name, :username, :bio])
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show], param: :username, path: ''
   root to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # verb 'path', to: "controller#action"
+
 end
